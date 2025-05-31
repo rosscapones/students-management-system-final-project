ba089b2e1aac528562b746d8e40e535c68cca0b0
rosscapones <rosscapones@g.cjc.edu.ph>
2025-05-30 17:59:00 -0700

    Add files via upload

---
+++   Student Management System/src/student/management/system/connector.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student.management.system;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author USER
+ */
+public class connector {
+     private static Connection Myconnection;
+
+    public static void init() {
+        try {
+
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+
+                System.out.println("Class not found");
+            }
+            Myconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false", "root", "bros31");
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        }
+    }
+
+    public static Connection getConnection() {
+        if (Myconnection != null) {
+            return Myconnection;
+        } else {
+            try {
+
+                Myconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/store?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false", "root", "");
+
+            } catch (Exception ex) {
+
+            }
+        }
+        return Myconnection;
+
+    }
+
+    public static void close(ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+    }
+}
+
+
